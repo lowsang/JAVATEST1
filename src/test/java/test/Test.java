c32e68c7cfa826f91c517b760d883261adb1552c
lowsang，这是我的名字 <49596168@qq.com>
2019-12-13 09:37:23 +0800

    第二次测试

---
+++ src/test/java/test/Test.java
@@ -0,0 +1,65 @@
+package test;
+
+public class Test {
+    public static void main(String[] args){
+//        String str1="hello";
+//        String str2="hello";
+//        String str3="hello"+"world";
+//        String str4=str2+"world";
+//        String str5=new String("hello");
+//        String str6=new String("hello");
+//        String str7=str6.intern();
+//        String str8=new String("hello").intern();
+//       // System.out.println(str1==str2);
+//// System.out.println(str3==str4);
+////      System.out.println(str1==str5);
+////       System.out.println(str5==str6);
+//  System.out.println(str1==str7);
+//  System.out.println(str1==str8);
+//        String str9="hello";
+//        str9+="world";
+//        System.out.println(str3==str9);
+//        System.out.printf("hello, %s %n", "world");
+//        System.out.printf("大写a：%c %n", 'A');
+//        System.out.printf("100 > 50：%b %n", 100 > 50);
+//        System.out.printf("100除以2：%d %n", 100 / 2);
+//        System.out.printf("100的16进制数是：%x %n", 100);
+//        System.out.printf("100的8进制数是：%o %n", 100);
+//        System.out.printf("100元打8.5折扣是：%f 元%n", 50 * 0.85);
+//        System.out.printf("上述价格的16进制数是：%a %n", 50 * 0.85);
+//        System.out.printf("上面的折扣是%d%% %n", 85);
+//        String str = "hello, world!";
+//        System.out.println(str.indexOf("e"));
+//        System.out.println(str.indexOf('e'));
+//        System.out.println(str.indexOf(101));
+//
+//        System.out.println(str.indexOf("o", 5));
+//
+//        System.out.println(str.indexOf("l"));
+//        System.out.println(str.lastIndexOf("l"));
+//        System.out.println(str.lastIndexOf("l", 9));
+//        System.out.println("hello, world!".replace('o', 'A'));
+//        System.out.println("hello, world!".replace("o", "Aaaa"));
+//        System.out.println("hello, world!".replaceAll("o", "OOO"));
+//        System.out.println("hello, world!".replaceAll("o", "s"));
+       // System.out.println("        hello,world!       ".trim());
+//        String str = "hello, world!";
+//
+//        System.out.println(str.substring(1));
+//
+//        System.out.println(str.substring(0, 1));
+//        System.out.println(str.substring(0, str.length() - 1));
+//        //System.out.println(str.substring(0, str.length() + 1));
+        System.out.println("F".equalsIgnoreCase("f"));
+
+    }
+
+    @org.testng.annotations.Test
+    public void testName() {
+    }
+
+    @org.testng.annotations.Test
+    public void testmy() {
+    }
+
+}
---
+++ src/test/java/test/test/TestTest.java
@@ -0,0 +1,38 @@
+package test.test; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Test Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>12月 13, 2019</pre> 
+* @version 1.0 
+*/ 
+public class TestTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+}
+
+    @Test
+    public void showname() {
+
+    }
+}
